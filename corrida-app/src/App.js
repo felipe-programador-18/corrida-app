c3417a4851d0a798ce92b1facabaf00d87abe2b2
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-03 06:09:19 -0300

    training more react always

---
+++ corrida-app/src/App.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import Button from './button';
+import './App.css';
+
+function App() {
+
+//practice about useState
+  const [Maisone, setAumenta] = useState(0)
+  const Increased = () => {
+     setAumenta(Maisone+1)
+     console.log('test here')
+  }
+ const Decrement = () => {
+   setAumenta(Maisone-1)
+ }
+
+ //here is tagget element of set in the choise!!
+ const [brand, setBrand] = useState('')
+
+ // work local storage!!////////////////
+ const [valor, setValor] = useState('')
+const Addinging = (chave, valor) =>{
+  localStorage.setItem(chave,valor)
+}
+
+const Busca = (chave) =>{
+  localStorage.getItem( chave)
+  alert('Guardei name' + valor)
+}
+
+const Apaga = (chave) =>{
+  localStorage.removeItem(chave)
+}
+//////////////////////////////////////
+
+const Aumenta = (props) =>{
+    return (
+      <p>
+       {props.more}
+         </p>
+       )
+  }
+  const Caught = (props) =>{
+   return(
+    <h1>
+   {props.text}
+   </h1>
+   )
+  }
+
+ const Paragrafo = (props) =>{
+   return(
+     <p>
+     {props.text}
+     </p>
+   )
+ }
+
+ //here i get value of option!!
+const Pegaopt = (props) =>{
+  return(
+    <option>{props.value}</option>
+  )
+}
+
+
+
+
+  return (
+    <div className="App">
+    
+    <Caught text='let is starte now!!!'/>
+
+    <Paragrafo text = 'testando now!!!' />
+    
+   
+    <Aumenta more={Maisone} />
+    <Button text ='+' onClick={Increased}  />
+    <Button text='-' onClick={Decrement} /><br/>
+   
+    
+    <select muda={brand}  onChange={(e) => setBrand(e.target.value)}  >
+    
+    <Pegaopt value='ferrari'/>
+    <Pegaopt value='lamburguini'/>
+    <Pegaopt value='camaro'  />
+    <Pegaopt value='renalt' />
+    </select>
+    <Paragrafo text= {'you select: ' + brand} />
+
+
+
+    <label> Escreva um nome :</label>
+    <input type='text'   value ={valor}      onChange={(e) => setValor(e.target.value)}  />
+    <Button text='armazena' onClick={() => Addinging('ls_name', valor)}       />
+    <Button text ='busca' onClick={() => Busca('ls_name')} />
+    <Button text ='Deleta' onClick={() => Apaga('ls_name')} />
+   
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ corrida-app/src/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+ // i have remember when i passo the referrer i have that adding button and onclick!!
+const Button = (props) => <button onClick={props.onClick}>{props.text} </button>
+
+
+
+
+
+export default Button
