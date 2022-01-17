6b4fa6865876a76f7cc1ddef7027ae7a19fe3586
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-17 14:53:12 -0300

    c14-adding new modify about react

--- corrida-app/src/create.js
+++ corrida-app/src/create.js
@@ -2,12 +2,6 @@ import React from "react";
 
 
 
-const Create = (props) =>{ <button onClick={props.onClick}>{props.more}</button>
-    return(
-        <button>{props.missed}</button>
-    )
-}
-
-
-
+const Create = (props) => <button onClick={props.onClick}>{props.missed}</button>
+    
 export default Create
