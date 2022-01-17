e24198e630a7e2266dced736b5333739018d3a5f
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-17 14:49:18 -0300

    c13- export button create

--- corrida-app/src/create.js
+++ corrida-app/src/create.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 
-const Create = (props) =>{
+const Create = (props) =>{ <button onClick={props.onClick}>{props.more}</button>
     return(
         <button>{props.missed}</button>
     )
